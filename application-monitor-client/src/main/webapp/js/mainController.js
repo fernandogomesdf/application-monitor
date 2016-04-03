ef73d5cc7d3acc4adf35020c86d6d1dc710696db
Fernando <Fernando@192.168.25.238>
2016-04-03 13:58:05 -0300

    adding test data

--- application-monitor-client/src/main/webapp/js/mainController.js
+++ application-monitor-client/src/main/webapp/js/mainController.js
@@ -3,40 +3,48 @@ angular.module('app').controller('MainCtrl', function($scope, $window, $interval
 	// three servers with same values for testing purposes. It should be changed with real values.
 	$scope.servers = {
 		"values" : [
-				{
-					"name" : "localhost",
-					"url" : "http://localhost:8080/application-monitor-server/services/status/serverStatus"
-				},
-				{
-					"name" : "localhost",
-					"url" : "http://localhost:8080/application-monitor-server/services/status/serverStatus"
-				},
-				{
-					"name" : "localhost",
-					"url" : "http://localhost:8080/application-monitor-server/services/status/serverStatus"
-				}]
+			{
+				"name" : "localhost 1",
+				"url" : "http://localhost:8080/application-monitor-server/services/status/serverStatus"
+			},
+			{
+				"name" : "localhost 2",
+				"url" : "http://localhost:8080/application-monitor-server/services/status/serverStatus"
+			},
+			{
+				"name" : "localhost 3",
+				"url" : "http://localhost:8080/application-monitor-server/services/status/serverStatus"
+			}]
 	};
 	
-	for ( var i = 0 ; i < $scope.servers.values.length ; i++ ) {
-		$scope.servers.values[i].id = i;
-		$http.post($scope.servers.values[i].url, $scope.servers.values[i]).success(function(data, status) {
-			// replace values in array with server values
-			for ( var j = 0 ; j < $scope.servers.values.length ; j++ ) { 
-				if (j == data.id) {
-					$scope.servers.values[j] = data;
-					break;
+	$scope.checkServers = function() {
+		for ( var i = 0 ; i < $scope.servers.values.length ; i++ ) {
+			$scope.servers.values[i].id = i;
+			$http.post($scope.servers.values[i].url, $scope.servers.values[i]).success(function(data, status) {
+				// replace values in array with server values
+				for ( var j = 0 ; j < $scope.servers.values.length ; j++ ) { 
+					if (j == data.id) {
+						$scope.servers.values[j] = data;
+						break;
+					}
 				}
-			}
-	    }).error(function(data, status, headers, config) {
-	    	// replace values in array with server values
-	    	for ( var j = 0 ; j < $scope.servers.values.length ; j++ ) { 
-	    		if (j == config.data.id) {
-					$scope.servers.values[j].status = 3; // red alert
-					$scope.servers.values[j].serverStatus = status; 
-					$scope.servers.values[j].data = data; // error message from the server
-					break;
-				}
-	    	}
-	    });
+		    }).error(function(data, status, headers, config) {
+		    	// replace values in array with server values
+		    	for ( var j = 0 ; j < $scope.servers.values.length ; j++ ) { 
+		    		if (j == config.data.id) {
+						$scope.servers.values[j].status = 3; // red alert
+						$scope.servers.values[j].serverStatus = status; 
+						$scope.servers.values[j].data = data; // error message from the server
+						break;
+					}
+		    	}
+		    });
+		}
 	}
+	
+	$scope.checkServers();
+	$interval(function(){
+		$scope.checkServers();
+	}, 500);
+	
 });
--- application-monitor-server/src/main/java/in/summtech/application/monitor/server/rest/StatusRest.java
+++ application-monitor-server/src/main/java/in/summtech/application/monitor/server/rest/StatusRest.java
@@ -2,6 +2,7 @@ package in.summtech.application.monitor.server.rest;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import javax.json.JsonObject;
 import javax.ws.rs.GET;
@@ -23,11 +24,64 @@ public class StatusRest {
 	@POST
 	@Path("/serverStatus")
 	public Response serverStatus(JsonObject data) {
-		return buildResponse(new WebServer(data.getInt("id"), data.getString("name"), data.getString("url"), 1, "teste", new ArrayList<Service>()));
+
+		// Implement your code here - begin
+
+		int simulatedStatus1 = simulateStatus();
+		Service service1 = new Service("Oracle", "java:comp/env/jdbc/OracleDataSource", simulatedStatus1,
+				getSimulatedMessage(simulatedStatus1));
+		
+		int simulatedStatus2 = simulateStatus();
+		Service service2 = new Service("MySQL", "java:comp/env/jdbc/MySQLDataSource", simulatedStatus2,
+				getSimulatedMessage(simulatedStatus2));
+		
+		int simulatedStatus3 = simulateStatus();
+		Service service3 = new Service("LDAP", "ldap://localhost:389/cn=homedir,cn=Jon%20Ruiz,ou=people,o=JNDITutorial", simulatedStatus3,
+				getSimulatedMessage(simulatedStatus3));
+
+		int simulatedWebStatus = simulateStatus();
+		WebServer webserver = new WebServer(data.getInt("id"), data.getString("name"), data.getString("url"),
+				simulatedWebStatus, getSimulatedMessage(simulatedWebStatus), new ArrayList<Service>());
+		
+		webserver.getServices().add(service1);
+		webserver.getServices().add(service2);
+		webserver.getServices().add(service3);
+		
+		// Implement your code here - end
+
+		return buildResponse(webserver);
 	}
-	
+
+	private int simulateStatus() {
+		Random rand = new Random();
+		int max = 3;
+		int min = 1;
+		int randomNum = rand.nextInt((max - min) + 1) + min;
+		return randomNum;
+	}
+
+	private String getSimulatedMessage(int status) {
+		String message = null;
+		switch (status) {
+		case 1:
+			message = "Ok";
+			break;
+		case 2:
+			message = "Slow";
+			break;
+		case 3:
+			message = "Not Working";
+			break;
+		default:
+			message = "Unknow failure";
+			break;
+		}
+		return message;
+	}
+
 	/**
 	 * Build the response for the client
+	 * 
 	 * @param entity
 	 * @return
 	 */
@@ -37,10 +91,11 @@ public class StatusRest {
 		Response response = responseBuilder.build();
 		return response;
 	}
-	
+
 	/**
-	 * Represents the WebServer to be monitored
-	 * Innner class for explanatory purposes
+	 * Represents the WebServer to be monitored Innner class for explanatory
+	 * purposes
+	 * 
 	 * @author Fernando
 	 */
 	public class WebServer {
@@ -48,18 +103,26 @@ public class StatusRest {
 		private String name;
 		private String url;
 		private Integer status; // 1 - fully functional (green) / 2 - some
-								// service stoped or minor problem (orange) / 3 - some serious
+								// service stoped or minor problem (orange) / 3
+								// - some serious
 								// problem detected (red)
 		private String msg;
 		private List<Service> services;
-		
+
 		/**
 		 * Default constructor
-		 * @param name name of this webservice (better receive from the client because it could be running in many servers)
-		 * @param url url of this webservice (better receive from the client because it could be running in many servers)
-		 * @param status service status from 1 to 3
-		 * @param msg 
-		 * @param services like databases, elasticsearch or jcr
+		 * 
+		 * @param name
+		 *            name of this webservice (better receive from the client
+		 *            because it could be running in many servers)
+		 * @param url
+		 *            url of this webservice (better receive from the client
+		 *            because it could be running in many servers)
+		 * @param status
+		 *            service status from 1 to 3
+		 * @param msg
+		 * @param services
+		 *            like databases, elasticsearch or jcr
 		 */
 		public WebServer(Integer id, String name, String url, Integer status, String msg, List<Service> services) {
 			super();
@@ -70,11 +133,11 @@ public class StatusRest {
 			this.msg = msg;
 			this.services = services;
 		}
-		
+
 		public Integer getId() {
 			return id;
 		}
- 		
+
 		public void setId(Integer id) {
 			this.id = id;
 		}
@@ -119,27 +182,34 @@ public class StatusRest {
 			this.services = services;
 		}
 	}
-	
+
 	/**
-	 * Some service for monitoring like Database, ElasticSearch or JCR
-	 * Innner class for explanatory purposes
+	 * Some service for monitoring like Database, ElasticSearch or JCR Innner
+	 * class for explanatory purposes
+	 * 
 	 * @author Fernando
 	 *
 	 */
 	public class Service {
-		
+
 		private String name;
 		private String url;
-		private Integer status; // 1 - fully functional (green) / 2 - slow or problem detected
+		private Integer status; // 1 - fully functional (green) / 2 - slow or
+								// problem detected
 								// (orange) / 3 - stopped (red)
 		private String msg;
-		
+
 		/**
-		 * Default constructor 
-		 * @param name name of this service associated to the webserver
-		 * @param url url of this service
-		 * @param status from 1 to 3
-		 * @param msg to describe failure or success
+		 * Default constructor
+		 * 
+		 * @param name
+		 *            name of this service associated to the webserver
+		 * @param url
+		 *            url of this service
+		 * @param status
+		 *            from 1 to 3
+		 * @param msg
+		 *            to describe failure or success
 		 */
 		public Service(String name, String url, Integer status, String msg) {
 			super();
@@ -182,3 +252,4 @@ public class StatusRest {
 		}
 	}
 }
+
